6b29aad57eb079786d63742c12d3225edd39a8c5
Faris <farissharayri@gmail.com>
2024-09-16 07:39:43 -0400

    exrecise 2

--- exercise.py
+++ exercise.py
@@ -39,14 +39,47 @@ print_greeting()
 def check_letter():
     # Your control flow logic goes here
     vowels = ('a','e','i','o','u') # made this into a set not a list (immutable in nature and no duplicates)
-    user_input = input("enter a letter (a-z or A-Z)").lower() # .lower to check easily later on 
+    user_input = input("enter a letter (a-z or A-Z) ").lower() # .lower to check easily later on 
     if user_input in vowels: 
       print(f'The letter {user_input} is a vowel.')
     else:
       print(f'The letter {user_input} is a consonant.')
-      
 
 # Call the function
 check_letter()
 
 
+# Exercise 2: Old enough to vote?
+#
+# Write a Python function named `check_voting_eligibility` that determines if a user is old enough to vote.
+# Fill in the logic to perform the eligibility check inside the function.
+#
+# Function Details:
+# - Prompt the user to input their age: "Please enter your age: "
+# - Validate the input to ensure the age is a possible value (no negative numbers).
+# - Determine if the user is eligible to vote. Set a variable for the voting age.
+# - Print a message indicating whether the user is eligible to vote based on the entered age.
+#
+# Hints:
+# - Use the `input()` function to capture the user's age.
+# - Use `int()` to convert the input to an integer. Ensure to handle any conversion errors gracefully.
+# - Use a conditional statement to check if the age meets the minimum voting age requirement.
+
+def check_voting_eligibility():
+    # Your control flow logic goes here
+    voting_age = 21
+    age = input("Please enter your age: ")
+    try:
+      age = int(age)
+    except:
+      age = int(input(f"{age} is not a valid age, Please enter a number: "))
+    if age not in range(2,110): # optimistic thinking on the 110 age limit , also optimistic thinking in finding a genius thats trying this at 3 years-old
+      print(f'invalid age input {age}')
+      age=int(input('enter a valid age between 3 and 109: '))
+    if age>=voting_age:
+      print('you are eligible to vote')
+    else:
+      print('you are not eligible to vote')
+
+# Call the function
+check_voting_eligibility()
